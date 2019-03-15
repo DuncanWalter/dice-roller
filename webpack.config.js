8ce0be8d19449166608a071688d0190f4c44a7ed
DuncanWalter <dwalter@barlingarts.com>
2019-03-15 08:55:58 -0500

    maybe this time...

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ function createConfig(production) {
       path: outPath,
       filename: 'bundle.js',
       chunkFilename: '[id].chunk.js',
-      publicPath: '/',
+      publicPath: './',
     },
     target: 'web',
     resolve: {
